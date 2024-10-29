69011fcc109ecb5773e7a83d640f97005a3419bd
julia albuquerque <julialbuquerquee31@gmail.com>
2024-10-29 12:14:59 -0300

    .

--- grafico/informacoesGlobais.js
+++ grafico/informacoesGlobais.js
@@ -1,5 +1,6 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-async function vizualizarInformacoesglobais() {
+
+async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
@@ -16,4 +17,4 @@ async function vizualizarInformacoesglobais() {
     container.appendChild(paragrafo)
 }
 
-vizualizarInformacoesglobais()
+vizualizarInformacoesGlobais()
